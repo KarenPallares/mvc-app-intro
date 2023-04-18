033fd1be4cada7d13904fbde6f442b4fd0051dd1
KarenPallares <krpallares@uts.edu.co>
2023-04-17 19:07:21 -0500

    Primer  push

---
+++ src/test/java/com/uts_api/MvcAppIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAppIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
